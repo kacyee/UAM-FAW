a1c5a9439af6bdca6389004c57b5b9fbe42dffd4
Patryk <ikacyee@gmail.com>
2019-01-23 23:50:38 +0100

    Trying to fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyAlmRAExTcHHxSvmVUQFD-9aHnY1-Nwn60",
     authDomain: "fawapplication.firebaseapp.com",
